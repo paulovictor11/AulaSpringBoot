d34c7bcc85bbc75cbdc73ff6401ff563cd3944df
paulovictor11 <pauloalmeidajp652@gmail.com>
2018-03-15 11:08:26 -0300

    Primeiro SpringBoot

---
+++ src/main/java/com/aulapablo/p5spring/P5AulaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.aulapablo.p5spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P5AulaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P5AulaSpringBootApplication.class, args);
+	}
+}
